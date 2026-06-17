f6060ab1bd22f6e8849739277e4e924f3fdda377
Antonio Natusch <anthony2004951@gmail.com>
2026-06-17 13:08:56 -0400

    fix(config): change order of plugins declaration
    TODO: make mobile version work

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": `${process.cwd()}/src`,
+      '@': `${process.cwd()}/src`,
     },
   },
   plugins: [
     tanstackStart({
       spa: { enabled: true },
     }),
+    react(),
     tailwindcss(),
   ],
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
 });
